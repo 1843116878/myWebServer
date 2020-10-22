6718165775e64453efed2a7424ab9e0c67678869
yuanzhihong <1843116878@qqcom>
2020-10-22 19:06:46 +0800

    ‘thread’

--- net/base/Util.cpp
+++ net/base/Util.cpp
@@ -37,19 +37,100 @@ ssize_t readn(int fd, void *buff, size_t n) {
 }
 
 ssize_t readn(int fd, std::string &inBuffer, bool &zero) {
-    return 0;
+    ssize_t nread = 0, readSum = 0;
+    while (true){
+        char buff[MAX_BUFF];
+        if((nread = read(fd, buff, MAX_BUFF)) < 0) {
+            if (errno == EINTR)
+                continue;
+            else if (errno == EAGAIN)
+                return readSum;
+            else {
+                perror("read error");
+                return -1;
+            }
+        }
+        else if(nread == 0){
+            zero = true;
+            break;
+        }
+        readSum += nread;
+        inBuffer += std::string (buff, buff + nread);
+    }
+    return readSum;
 }
 
 ssize_t readn(int fd, std::string &inBuffer) {
-    return 0;
+    size_t nread = 0, readSum = 0;
+    while (true){
+        char buff[MAX_BUFF];
+        if((nread = read(fd, buff, MAX_BUFF)) < 0){
+            if(errno == EINTR)
+                continue;
+            else if(errno == EAGAIN)
+                return readSum;
+            else{
+                perror("read error");
+                return -1;
+            }
+        }
+        else if(nread == 0)
+            break;
+        readSum += nread;
+        inBuffer += std::string(buff, buff + nread);
+    }
+    return readSum;
 }
 
 ssize_t writen(int fd, void *buff, size_t n) {
-    return 0;
+    size_t nleft = n, nwritten = 0, writeSum = 0;
+    char *ptr =(char *)buff;
+    while (nleft > 0){
+        if((nwritten = write(fd, ptr, nleft)) <= 0){
+            if(nwritten < 0){
+                if(errno == EINTR){
+                    nwritten = 0;
+                    continue;;
+                }
+                else if(errno == EAGAIN)
+                    return writeSum;
+                else
+                    return -1;
+            }
+        }
+        writeSum += nwritten;
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return writeSum;
 }
 
 ssize_t writen(int fd, std::string &sbuff) {
-    return 0;
+    size_t nleft = sbuff.size(), nwritten  = 0;
+    size_t writeSum = 0;
+    const char *ptr = sbuff.c_str();
+    while (nleft > 0){
+        if((nwritten = write(fd, ptr, nleft)) <= 0){
+            if(nwritten < 0){
+                if(errno == EINTR){
+                    nwritten = 0;
+                    continue;
+                }
+                else if(errno == EAGAIN)
+                    break;
+                else
+                    return -1;
+            }
+        }
+        writeSum += nwritten;
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    if(writeSum == static_cast<int>(sbuff.size()))
+        sbuff.clear();
+    else
+        sbuff = sbuff.substr(writeSum);
+    return writeSum;
 }
 
 void handle_for_sigpipe() {
