373c751a6e15a9e5b7833551183326657651e16a
yuanzhihong <1843116878@qqcom>
2020-10-23 18:00:47 +0800

    ‘base logging’

---
+++ net/base/AsyncLogging.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yuanzhihong on 2020/10/23.
+//
+
+#include "AsyncLogging.h"
---
+++ net/base/AsyncLogging.h
@@ -0,0 +1,14 @@
+//
+// Created by yuanzhihong on 2020/10/23.
+//
+
+#ifndef MYWEBSERVER_ASYNCLOGGING_H
+#define MYWEBSERVER_ASYNCLOGGING_H
+
+
+class AsyncLogging {
+
+};
+
+
+#endif //MYWEBSERVER_ASYNCLOGGING_H
