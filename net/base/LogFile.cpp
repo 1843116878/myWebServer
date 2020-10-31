b66f44c2e6224a6e92ab6044d9faa7de5c0fe364
yuanzhihong <1843116878@qqcom>
2020-10-31 18:04:31 +0800

    'LogFile'

--- net/base/LogFile.cpp
+++ net/base/LogFile.cpp
@@ -2,3 +2,37 @@
 // Created by yuanzhihong on 2020/10/22.
 //
 
+#include "LogFile.h"
+#include <cassert>
+#include <memory>
+
+LogFile::LogFile(const std::string &basename, int flushEveryN)
+    :basename_(basename),
+    flushEveryN_(flushEveryN),
+    count_(0),
+    mutex_(){
+    file_ = std::make_unique<AppendFile>(basename);
+}
+
+LogFile::~LogFile() {}
+
+void LogFile::append(const char *logline, int len) {
+    std::lock_guard<std::mutex> lockGuard(mutex_);
+    append_unlocked(logline, len);
+}
+
+void LogFile::flush() {
+    std::lock_guard<std::mutex> lockGuard(mutex_);
+    file_->fluse();
+}
+
+
+void LogFile::append_unlocked(const char *logline, int len) {
+    file_->append(logline, len);
+    ++count_;
+    if(count_ >= flushEveryN_){
+        count_ = 0;
+        file_->fluse();
+    }
+}
+
--- net/base/LogFile.h
+++ net/base/LogFile.h
@@ -5,4 +5,29 @@
 #ifndef MYWEBSERVER_LOGFILE_H
 #define MYWEBSERVER_LOGFILE_H
 
+#include <mutex>
+#include <memory>
+#include <string>
+#include "FileUtil.h"
+
+class LogFile{
+public:
+    LogFile(const LogFile&) = delete;
+    const LogFile& operator=(const LogFile&)=delete;
+    // 每被append flushEveryN次，flush一下，会往文件写，只不过，文件也是带缓冲区的
+    LogFile(const std::string &basename, int flushEveryN = 1024);
+    ~LogFile();
+
+public:
+    void append(const char *logline, int len);
+    void flush();
+private:
+    void append_unlocked(const char *logline, int len);
+
+    const std::string basename_;
+    const int flushEveryN_;
+    int count_;
+    std::mutex mutex_;
+    std::unique_ptr<AppendFile> file_;
+};
 #endif //MYWEBSERVER_LOGFILE_H
