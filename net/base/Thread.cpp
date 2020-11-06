ee84fce41d32bd8e3ede45e74f80545c8784e3f4
yuanzhihong <1843116878@qqcom>
2020-11-06 17:24:35 +0800

    'Thread'

--- net/base/Thread.cpp
+++ net/base/Thread.cpp
@@ -1,49 +1,50 @@
-//
-// Created by 袁志红 on 2020/10/18.
-//
 #include "Thread.h"
-#include "CurrentThread.h"
 #include <assert.h>
-#include <utility>
-#include <zconf.h>
-#include <syscall.h>
+#include <errno.h>
+#include <linux/unistd.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <sys/prctl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <memory>
+#include "CurrentThread.h"
 
 
-namespace CurrentThread
-{
+namespace CurrentThread {
     __thread int t_cachedTid = 0;
     __thread char t_tidString[32];
     __thread int t_tidStringLength = 6;
     __thread const char* t_threadName = "default";
-    void cacheTid() {
-        if (t_cachedTid == 0) {
-            t_cachedTid = gettid();
-            t_tidStringLength =
-                    snprintf(t_tidString, sizeof t_tidString, "%5d ", t_cachedTid);
-        }
+}
+
+pid_t gettid() { return static_cast<pid_t>(::syscall(SYS_gettid)); }
+
+void CurrentThread::cacheTid() {
+    if (t_cachedTid == 0) {
+        t_cachedTid = gettid();
+        t_tidStringLength =
+                snprintf(t_tidString, sizeof t_tidString, "%5d ", t_cachedTid);
     }
 }
-pid_t gettid(){
-    return static_cast<pid_t>(::syscall(SYS_gettid));
-};
-//保存线程中的name和tid
+
+// 为了在线程中保留name,tid这些数据
 struct ThreadData {
-    using ThreadFunc = Thread::ThreadFunc;
+    typedef Thread::ThreadFunc ThreadFunc;
     ThreadFunc func_;
     std::string name_;
-    pid_t *tid_;
-    CountDownLatch *latch_;
+    pid_t* tid_;
+    CountDownLatch* latch_;
 
-    ThreadData(const ThreadFunc &func, const std::string &name, pid_t *tid,
-               CountDownLatch *latch)
+    ThreadData(const ThreadFunc& func, const std::string& name, pid_t* tid,
+               CountDownLatch* latch)
             : func_(func), name_(name), tid_(tid), latch_(latch) {}
 
     void runInThread() {
         *tid_ = CurrentThread::tid();
-        tid_ = nullptr;
+        tid_ = NULL;
         latch_->countDown();
-        latch_ = nullptr;
+        latch_ = NULL;
 
         CurrentThread::t_threadName = name_.empty() ? "Thread" : name_.c_str();
         prctl(PR_SET_NAME, CurrentThread::t_threadName);
@@ -53,41 +54,52 @@ struct ThreadData {
     }
 };
 
-void* startThread(void *obj) {
-    ThreadData *data = static_cast<ThreadData *>(obj);
+void* startThread(void* obj) {
+    ThreadData* data = static_cast<ThreadData*>(obj);
     data->runInThread();
     delete data;
-    return nullptr;
+    return NULL;
 }
 
+Thread::Thread(const ThreadFunc& func, const std::string& n)
+        : started_(false),
+          joined_(false),
+          thread_(),
+          tid_(0),
+          func_(func),
+          name_(n),
+          latch_(1) {
+    setDefaultName();
+}
 
-int Thread::join() {
-    assert(started_);
-    assert(!joined_);
-    if (thread_.joinable()) {
-        joined_ = true;
-        thread_.join();
-    } else
-        return -1;
-    return 0;
+Thread::~Thread() {
+    if (started_ && !joined_)
+        thread_.detach();
+}
+
+void Thread::setDefaultName() {
+    if (name_.empty()) {
+        char buf[32];
+        snprintf(buf, sizeof buf, "Thread");
+        name_ = buf;
+    }
 }
 
 void Thread::start() {
     assert(!started_);
     started_ = true;
-    ThreadData *data = new ThreadData(func_, name_, &tid_, &latch_);
+    ThreadData* data = new ThreadData(func_, name_, &tid_, &latch_);
     thread_ = std::thread(startThread, data);
-    started_ = false;
-    delete data;
+    latch_.wait();
 }
 
-Thread::Thread(const ThreadFunc &func, std::string name)
-        : thread_(), func_(func), name_(std::move(name)), id_(0), tid_(0), started_(false), joined_(false), latch_(1) {}
-
-void Thread::setDefaultName() {
-    if (name_.empty()){
-        char buf[32];
-        snprintf(buf, sizeof buf, "Thread");
+int Thread::join() {
+    assert(started_);
+    assert(!joined_);
+    joined_ = true;
+    if(thread_.joinable()){
+        thread_.join();
     }
+    else
+        return -1;
 }
-
--- net/base/Thread.h
+++ net/base/Thread.h
@@ -1,39 +1,32 @@
-//
-// Created by 袁志红 on 2020/10/18.
-//
-
-#ifndef MYWEBSERVER_THREAD_H
-#define MYWEBSERVER_THREAD_H
-
+#pragma once
+#include <pthread.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 #include <functional>
+#include <memory>
+#include <string>
 #include <thread>
 #include "CountDownLatch.h"
 
 
-class Thread {
+class Thread   {
 public:
     using ThreadFunc = std::function<void()>;
-public:
-    explicit Thread(const ThreadFunc &ThreadFunc, std::string name = "");
-    Thread(const Thread &) = delete;
-    const Thread &operator=(const Thread &) = delete;
-public:
+    explicit Thread(const ThreadFunc&, const std::string& name = std::string());
+    ~Thread();
     void start();
     int join();
-public:
-    bool started() { return started_; };
-    bool joined() { return joined_; };
-    pid_t pid() { return tid_; };
-    std::string name() const { return name_; };
+    bool started() const { return started_; }
+    pid_t tid() const { return tid_; }
+    const std::string& name() const { return name_; }
+
 private:
     void setDefaultName();
+    bool started_;
+    bool joined_;
     std::thread thread_;
-    pid_t tid_;                 //进程id
-    std::thread::id id_;        //线程id
-    ThreadFunc func_;           //线程要执行的任务
-    bool started_;              //用来标记是否执行过
-    bool joined_;               //用来标记是否join过
+    pid_t tid_;
+    ThreadFunc func_;
     std::string name_;
     CountDownLatch latch_;
 };
-#endif //MYWEBSERVER_THREAD_H
