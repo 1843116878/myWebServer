2cf79754dfe05dbf69c88386dd37d3d86b85ecfe
yuanzhihong <1843116878@qqcom>
2020-11-17 17:43:18 +0800

    'Channel'

--- net/Channel.cpp
+++ net/Channel.cpp
@@ -1,69 +1,47 @@
+// Created by yuanzhihong
 //
-// Created by yuanzhihong on 2020/11/1.
-//
-
 #include "Channel.h"
+#include <unistd.h>
+#include "EventLoop.h"
+
 
 Channel::Channel(EventLoop *loop)
-    :loop_(loop), fd_(0), events_(0), lastEvents(0)
-{}
+        : loop_(loop), events_(0), lastEvents_(0), fd_(0) {}
 
 Channel::Channel(EventLoop *loop, int fd)
-    :loop_(loop), fd_(fd), events_(0),lastEvents(0)
-{}
+        : loop_(loop), fd_(fd), events_(0), lastEvents_(0) {}
 
-Channel::~Channel()
-{}
+Channel::~Channel() {}
 
-int Channel::getfd() {
-    return fd_;
-}
+int Channel::getFd() { return fd_; }
 
-void Channel::setfd(int fd) {
-    fd_ = fd;
-}
-
-std::shared_ptr<HttpData> Channel::getHolder() {
-    std::shared_ptr<HttpData> res(holder_.lock());
-    return res;
-}
-
-void Channel::setHolder(const std::shared_ptr<HttpData>& holder) {
-    holder_ = holder;
-}
-
-void Channel::setRevents(__uint32_t ev) {
-    revents_ = ev;
-}
-
-void Channel::setEvents(__uint32_t ev) {
-    events_ = ev;
-}
-
-__uint32_t &Channel::getEvents() {
-    return events_;
-}
-
-__uint32_t Channel::getLastEvents() {
-    return lastEvents;
-}
+void Channel::setFd(int fd) { fd_ = fd; }
 
 void Channel::handleRead() {
-    if(readHandler_)
+    if (readHandler_) {
         readHandler_();
+    }
 }
 
 void Channel::handleWrite() {
-    if(writeHandler_)
+    if (writeHandler_) {
         writeHandler_();
+    }
 }
 
-void Channel::handleError(int fd, int err_num, std::string short_msg)
-{}
-
 void Channel::handleConn() {
-    if(connHandler_)
-        connHandler_();
+  if (connHandler_) {
+    connHandler_();
+  }
+}
+
+void Channel::setHolder(std::shared_ptr<HttpData> holder) {
+    holder_ = holder;
+}
+
+std::shared_ptr<HttpData> Channel::getHolder() {
+    std::shared_ptr<HttpData> res = holder_.lock();
+    return res;
 }
 
 void Channel::setReadHandler(Channel::CallBack &&readHandler) {
@@ -82,28 +60,45 @@ void Channel::setConnHandler(Channel::CallBack &&connHandler) {
     connHandler_ = connHandler;
 }
 
-bool Channel::EqualAndUpdateLastEvents() {
-    bool res = (lastEvents == events_);
-    lastEvents = events_;
-    return res;
-}
-
 void Channel::handleEvents() {
     events_ = 0;
-    if ((revents_ & EPOLLHUP) && !(revents_ &EPOLLIN)){
+    if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)) {
         events_ = 0;
         return;
     }
-    if (revents_ & EPOLLERR){
-        if(errorHandler_)
-            errorHandler_();
+    if (revents_ & EPOLLERR) {
+        if (errorHandler_) errorHandler_();
         events_ = 0;
         return;
     }
-    if (revents_ & (EPOLLIN | EPOLLPRI | EPOLLRDHUP))
+    if (revents_ & (EPOLLIN | EPOLLPRI | EPOLLRDHUP)) {
         handleRead();
-    if(revents_ & EPOLLOUT)
+    }
+    if (revents_ & EPOLLOUT) {
         handleWrite();
+    }
     handleConn();
+}
+
+void Channel::setRevents(__uint32_t ev) {
+    revents_ = ev;
+}
+
+void Channel::setEvents(__uint32_t ev) {
+    events_ = ev;
+}
+
+__uint32_t &Channel::getEvents() {
+    return events_;
+}
+
+bool Channel::EqualAndUpdateLastEvents() {
+    bool ret = (lastEvents_ == events_);
+    lastEvents_ = events_;
+    return ret;
+}
+
+__uint32_t Channel::getLastEvents() {
+    return lastEvents_;
 }
 
--- net/Channel.h
+++ net/Channel.h
@@ -1,66 +1,70 @@
+// Created by yuanzhihong
 //
-// Created by yuanzhihong on 2020/11/1.
-//
-
-#ifndef MYWEBSERVER_CHANNEL_H
-#define MYWEBSERVER_CHANNEL_H
-
+#pragma once
+#include <sys/epoll.h>
 #include <sys/epoll.h>
 #include <functional>
-#include <unordered_map>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include "Timer.h"
 
 class EventLoop;
 class HttpData;
 
-class Channel{
+class Channel {
 public:
     using CallBack = std::function<void()>;
+public:
     Channel(EventLoop *loop);
     Channel(EventLoop *loop, int fd);
     ~Channel();
 
-    int getfd();
-    void setfd(int fd);
 
+    int getFd();
+    void setFd(int fd);
+
+    void setHolder(std::shared_ptr<HttpData> holder);
     std::shared_ptr<HttpData> getHolder();
-    void setHolder(const std::shared_ptr<HttpData>& holder);
 
     void setRevents(__uint32_t ev);
     void setEvents(__uint32_t ev);
-    __uint32_t &getEvents();
-    __uint32_t getLastEvents();
-
-
-    void handleRead();
-    void handleWrite();
-    void handleError(int fd, int err_num, std::string short_msg);
-    void handleConn();
 
     void setReadHandler(CallBack &&readHandler);
     void setWriteHandler(CallBack &&writeHandler);
     void setErrorHandler(CallBack &&errorHandler);
     void setConnHandler(CallBack &&connHandler);
 
-    bool EqualAndUpdateLastEvents();
-    //事件处理函数
     void handleEvents();
+    void handleRead();
+    void handleWrite();
+    void handleError(int fd, int err_num, std::string short_msg);
+    void handleConn();
+
+
+    __uint32_t &getEvents();
+    __uint32_t getLastEvents();
+
+    bool EqualAndUpdateLastEvents();
 private:
+    int parse_URI();
+    int parse_Headers();
+    int analysisRequest();
+
     CallBack readHandler_;
     CallBack writeHandler_;
     CallBack errorHandler_;
     CallBack connHandler_;
 private:
+
     EventLoop *loop_;
     int fd_;
     __uint32_t events_;
     __uint32_t revents_;
-    __uint32_t lastEvents;
+    __uint32_t lastEvents_;
+
     // 方便找到上层持有该Channel的对象
     std::weak_ptr<HttpData> holder_;
 };
-using SP_Channel = std::shared_ptr<Channel>;
 
-#endif //MYWEBSERVER_CHANNEL_H
+using SP_Channel = std::shared_ptr<Channel>;
