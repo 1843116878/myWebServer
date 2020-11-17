729ae610c83965f7612732cbdc0644c2ace9d1f8
yuanzhihong <1843116878@qqcom>
2020-11-17 20:01:21 +0800

    'EventLoopThreadPool'

--- net/EventLoopThreadPool.cpp
+++ net/EventLoopThreadPool.cpp
@@ -1,42 +1,37 @@
+// Created by yuanzhihong
 //
-// Created by yuanzhihong on 2020/11/4.
-//
-
-#include <cassert>
 #include "EventLoopThreadPool.h"
 
-
-EventLoopThreadPool::EventLoopThreadPool(EventLoop *baseLoop, int threadNums)
-    :baseLoop_(baseLoop), started_(false), threadNums_(threadNums), next_(0)
-{
-    if(threadNums <= 0) {
-        LOG << "threadNums <= 0";
-        abort();
-    }
+EventLoopThreadPool::EventLoopThreadPool(EventLoop *baseLoop, int numThreads)
+    : baseLoop_(baseLoop), started_(false), numThreads_(numThreads), next_(0) {
+  if (numThreads_ <= 0) {
+    LOG << "numThreads_ <= 0";
+    abort();
+  }
 }
 
 EventLoopThreadPool::~EventLoopThreadPool() {
-    LOG << "EventLoopThreadPoll() delete ";
+    LOG << "~EventLoopThreadPool()";
 }
 
 void EventLoopThreadPool::start() {
-    baseLoop_->assertInLoopThread();
-    started_ = true;
-    for (int i = 0; i < threadNums_; ++i) {
-        std::shared_ptr<EventLoopThread> t = std::make_shared<EventLoopThread>();
-        threads_.push_back(t);
-        loops_.push_back(t->startLoop());
-    }
+  baseLoop_->assertInLoopThread();
+  started_ = true;
+  for (int i = 0; i < numThreads_; ++i) {
+    std::shared_ptr<EventLoopThread> t(new EventLoopThread());
+    threads_.push_back(t);
+    loops_.push_back(t->startLoop());
+  }
 }
 
 EventLoop *EventLoopThreadPool::getNextLoop() {
-    baseLoop_->assertInLoopThread();
-    assert(started_);
-    EventLoop *loop = baseLoop_;
-    if(!loops_.empty()){
-        loop = loops_[next_];
-        next_ = (next_ + 1) % threadNums_;
-    }
-    return loop;
+  baseLoop_->assertInLoopThread();
+  assert(started_);
+  EventLoop *loop = baseLoop_;
+  if (!loops_.empty()) {
+    loop = loops_[next_];
+    next_ = (next_ + 1) % numThreads_;
+  }
+  return loop;
 }
 
--- net/EventLoopThreadPool.h
+++ net/EventLoopThreadPool.h
@@ -1,33 +1,29 @@
+// Created by yuanzhihong
 //
-// Created by yuanzhihong on 2020/11/4.
-//
-
-#ifndef MYWEBSERVER_EVENTLOOPTHREADPOOL_H
-#define MYWEBSERVER_EVENTLOOPTHREADPOOL_H
+#pragma once
 #include <memory>
 #include <vector>
 #include "EventLoopThread.h"
 #include "base/Logging.h"
 
 
-class EventLoopThreadPool{
+
+class EventLoopThreadPool  {
 public:
-    EventLoopThreadPool(const EventLoopThreadPool&) = delete;
-    const EventLoopThreadPool& operator= (const EventLoopThreadPool&) = delete;
+    EventLoopThreadPool(const EventLoopThreadPool&)=delete;
+    const EventLoopThreadPool& operator=(const EventLoopThreadPool&)=delete;
 public:
-    EventLoopThreadPool(EventLoop *baseLoop, int threadNums);
+    EventLoopThreadPool(EventLoop *baseLoop, int numThreads);
     ~EventLoopThreadPool();
+
     void start();
     EventLoop *getNextLoop();
+
 private:
     EventLoop *baseLoop_;
     bool started_;
-    int threadNums_;
+    int numThreads_;
     int next_;
-    std::vector<std::shared_ptr<EventLoopThread>>threads_;
-    std::vector<EventLoop*> loops_;
-
+    std::vector<std::shared_ptr<EventLoopThread>> threads_;
+    std::vector<EventLoop *> loops_;
 };
-
-
-#endif //MYWEBSERVER_EVENTLOOPTHREADPOOL_H
